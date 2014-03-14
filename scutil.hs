11c75d6b659c32e4af84bdcbbad6b4e0feb5c4f6
John Haugeland <stonecypher@gmail.com>
2014-03-14 01:20:58 -0700

    geo_mean w/ [] as 1

--- scutil.hs
+++ scutil.hs
@@ -1,5 +1,9 @@
 
 extrema (x:xs) = foldr (\y (lo, hi) -> (min y lo, max y hi)) (x, x) xs
+
 rotate_list n xs = drop l xs ++ take l xs where l = n `mod` length xs
+
 arith_mean xs = sum xs / (fromIntegral $ length xs)
-geo_mean = exp . arith_mean . map log
+
+geo_mean [] = 1.0; 
+geo_mean xs = exp . arith_mean . map log $ xs
