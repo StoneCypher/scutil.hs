b6b7e296a8a800541d6d957e91f9010ea001913b
John Haugeland <stonecypher@gmail.com>
2014-03-14 00:52:49 -0700

    improvement in notation

--- scutil.hs
+++ scutil.hs
@@ -2,4 +2,4 @@
 extrema (x:xs) = foldr (\y (lo, hi) -> (min y lo, max y hi)) (x, x) xs
 rotate_list n xs = drop l xs ++ take l xs where l = n `mod` length xs
 arith_mean xs = sum xs / (fromIntegral $ length xs)
-geo_mean xs = exp (arith_mean (map log xs))
+geo_mean = exp . arith_mean . map log
