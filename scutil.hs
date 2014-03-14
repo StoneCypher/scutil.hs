d629a09d078e1c906c6527174488f49f1e69b923
John Haugeland <stonecypher@gmail.com>
2014-03-14 00:51:21 -0700

    geometric mean

--- scutil.hs
+++ scutil.hs
@@ -1,3 +1,5 @@
 
 extrema (x:xs) = foldr (\y (lo, hi) -> (min y lo, max y hi)) (x, x) xs
 rotate_list n xs = drop l xs ++ take l xs where l = n `mod` length xs
+arith_mean xs = sum xs / (fromIntegral $ length xs)
+geo_mean xs = exp (arith_mean (map log xs))
